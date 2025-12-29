203a4e3358de9d16e221f0fa783ef467f1c4438c
Souravroy99 <souravroy200418@gmail.com>
2025-12-29 14:32:12 +0530

    Fix CSR issue

---
+++ components/LoginClient/LoginClient.jsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
+
+const LoginClient = () => {
+  const session = useSession();
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  useEffect(() => {
+    setError(params.get("error") || "");
+    setSuccess(params.get("success") || "");
+  }, [params]);
+
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (session.status === "authenticated") {
+    router.push("/dashboard");
+    return null;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", { email, password });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{success || "Welcome Back"}</h1>
+      <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input type="text" placeholder="Email" required className={styles.input} />
+        <input type="password" placeholder="Password" required className={styles.input} />
+        <button className={styles.button}>Login</button>
+        {error && <p>{error}</p>}
+      </form>
+
+      <button
+        onClick={() => signIn("google")}
+        className={`${styles.button} ${styles.google}`}
+      >
+        Login with Google
+      </button>
+
+      <span className={styles.or}>- OR -</span>
+
+      <Link className={styles.link} href="/dashboard/register">
+        Create new account
+      </Link>
+    </div>
+  );
+};
+
+export default LoginClient;
